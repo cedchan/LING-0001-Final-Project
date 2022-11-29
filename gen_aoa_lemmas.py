af48e5c7fac627c2106ec5949af35daf37ce037d
Cedric <cedchan@gmail.com>
2022-11-29 18:32:01 -0500

    AoA functionality update

---
+++ gen_aoa_lemmas.py
@@ -0,0 +1,11 @@
+import spacy
+import pandas as pd
+
+lemmatizer = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+
+df = pd.read_excel('reference/aoa/aoa_raw.xlsx')
+df = df.dropna()
+df.insert(1, 'Lemma', df['Word'])
+
+df['Lemma'] = df['Lemma'].map(lambda w: lemmatizer(w)[0].lemma_)
+df.to_csv('reference/aoa/aoa_lemmas.csv')
